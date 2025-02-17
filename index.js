043aa888ec00d3fcdbeae76b2724254dea56c08e
yashilbusa <yashilpatel1914@gmail.com>
2025-02-17 12:11:55 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import express from 'express';
 import mongoose from 'mongoose';
 
-await mongoose.connect("mongodb://localhost:27017/");
+const app = express()
+const port = 3012
 
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+}).listen(port,()=>{
+    console.log(`Server is Running on http://localhost:${port}`);
+})
+
+await mongoose.connect("mongodb://localhost:27017/");
