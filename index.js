f4161420716c162487a6bfc9c2d19fb618952f68
yashilbusa <yashilpatel1914@gmail.com>
2025-02-17 16:46:02 +0530

    commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = 3012
 
 config();
 
+app.use(express.json())
 // try{
 //     const connection = await mongoose.connect("mongodb://localhost:27017/todolist");
 //     console.log("Database Connected!!!");
@@ -32,9 +33,22 @@ try {
     console.error('Error connecting to MongoDB:', error.message);
 }
 
-app.get("/", async(req,res)=>{
-    const todo = new Todo({title:"Mongoose", desc:"Mongoose Package is Good", isDone:false})
-    await todo.save()
-    res.send("Hello World");
+app.get("/",(req,res)=>{
+    res.send("TodoList");
 })
 
+app.post("/addTask",async(req,res)=>{
+    const todo = new Todo({title:"Mongoose", desc:"Mongoose Package is Good", isDone:false})
+    await todo.save()
+})
+
+app.put("/updateTask/:id",async(req,res)=>{
+    const id = req.params.id;
+    const updatedData = req.body; 
+    await Todo.findByIdAndUpdate(id, updatedData);
+})
+
+app.delete("/delete/:id", async(req,res)=>{
+    const id = req.params.id;
+    await Todo.findByIdAndDelete(id);
+})
