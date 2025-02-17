3681a8d32eaaaecd4fa200464bdd336aade26332
yashilbusa <yashilpatel1914@gmail.com>
2025-02-17 16:15:17 +0530

    commit

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
+import dotenv from 'dotenv'
+import { config } from 'dotenv'
 import { Todo } from './models/Todo.js'
 
 const app = express()
 const port = 3012
 
-try{
-    const connection = await mongoose.connect("mongodb://localhost:27017/todolist");
-    console.log("Database Connected!!!");
-} catch(err){
-    console.log(err.messsage);
+config();
+
+// try{
+//     const connection = await mongoose.connect("mongodb://localhost:27017/todolist");
+//     console.log("Database Connected!!!");
+// } catch(err){
+//     console.log(err.messsage);
+// }
+
+
+// app.listen(port,()=>{
+//     console.log(`Server is Running on http://localhost:${port}`);
+// })
+
+try {
+    await mongoose.connect(process.env.URI);
+    console.log('Connected to MongoDB');
+    
+    app.listen(port, () => {
+      console.log(`Server Running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
 }
 
 app.get("/", async(req,res)=>{
@@ -18,6 +38,3 @@ app.get("/", async(req,res)=>{
     res.send("Hello World");
 })
 
-app.listen(port,()=>{
-    console.log(`Server is Running on http://localhost:${port}`);
-})
