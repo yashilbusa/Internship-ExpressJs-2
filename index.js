6f3d3391b4c6689ed756832c306538b7df7da6ab
yashilbusa <yashilpatel1914@gmail.com>
2025-02-17 12:48:26 +0530

    commiy

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { Todo } from './models/Todo.js'
 
 const app = express()
 const port = 3012
 
+const connection = await mongoose.connect("mongodb://localhost:27017/todolist");
 
 app.get("/",(req,res)=>{
-    res.send("Hello World")
-}).listen(port,()=>{
+    const todo = new Todo({title:"Mongoose", desc:"Mongoose Package is Good",isDone:true})
+    todo.save()
+    res.send("Hello World");
+})
+
+app.listen(port,()=>{
     console.log(`Server is Running on http://localhost:${port}`);
 })
 
-await mongoose.connect("mongodb://localhost:27017/");
