9812b9ff569f39383075d73dbef0203705ed61de
yashilbusa <yashilpatel1914@gmail.com>
2025-02-18 11:17:43 +0530

    commit

--- Aggregation Pipelines/top-5_FavouriteFruit.js
+++ Aggregation Pipelines/top-5_FavouriteFruit.js
@@ -1,1 +1,17 @@
-//List the top 5 common favourite fruits among the users
+//List the top common favourite fruits among the users
+db.test.aggregate([
+    {
+      '$group': {
+        '_id': '$favoriteFruit', 
+        'count': {
+          '$sum': 1
+        }
+      }
+    }, {
+      '$sort': {
+        'count': -1
+      }
+    }, {
+      '$limit': 1
+    }
+])
