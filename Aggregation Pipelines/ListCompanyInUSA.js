99c5a61fe255af5acbadfe9b081bc68eaaa90d42
yashilbusa <yashilpatel1914@gmail.com>
2025-02-19 12:18:50 +0530

    commit

--- Aggregation Pipelines/ListCompanyInUSA.js
+++ Aggregation Pipelines/ListCompanyInUSA.js
@@ -1,3 +1,11 @@
 //List all companies located in the USA with their corresponding user count 
 
-db.test.aggregate()
+db.test.aggregate([
+    {
+        '$match': {
+            'company.location.country': 'USA'
+        }
+    }, {
+        '$count': 'UserCount'
+    }
+])
